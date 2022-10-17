d889e2ac28bf9ff405bbe26f87e5181b33a84916
coxjoseph <cox.j@ufl.edu>
2022-10-17 16:43:19 -0400

    updated pretraining script to use singluarity

--- pretrain.sh
+++ pretrain.sh
@@ -1,5 +1,9 @@
-cd ~/GatorBrain || exit 1
+cd ~/GBT/GatorBrain || exit 1
 pwd; hostname; date
-module load conda
-conda activate /blue/ruogu.fang/cox.j/conda/envs/GatorBrain
-python GatorBrain.py
+
+module load singularity
+
+singularity exec --nv /blue/ruogu.fang/cox.j/monaicore.latest python -c "import torch; print(torch.cuda.is_available())"
+
+singularity exec --nv /blue/vendor-nvidia/hju/monaicore:latest python GatorBrain.py
+
