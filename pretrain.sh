91b30e63085fea537a151642036ed67584c65916
coxjoseph <cox.j@ufl.edu>
2022-10-18 15:35:26 -0400

    fix pretrain script

--- pretrain.sh
+++ pretrain.sh
@@ -4,6 +4,5 @@ pwd; hostname; date
 module load singularity
 
 singularity exec --nv /blue/ruogu.fang/cox.j/monaicore.latest python -c "import torch; print(torch.cuda.is_available())"
-
-singularity exec --nv /blue/vendor-nvidia/hju/monaicore:latest python GatorBrain.py
+singularity exec --nv --bind /red /blue/ruogu.fang/cox.j/monaicore.latest python GatorBrain.py
 
