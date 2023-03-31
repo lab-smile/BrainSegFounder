d497057178d81491c90bf50114afe5ed9740e462
Justin <jbroce@c0709a-s30.ufhpc>
2023-03-31 14:05:12 -0400

    removed large files

---
+++ code/finetune/.ipynb_checkpoints/finetune-checkpoint.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem-per-cpu=5gb
+#SBATCH --partition=gpu
+#SBATCH --gpus=a100:1
+#SBATCH --time=04:10:00
+#SBATCH --output=/blue/ruogu.fang/jbroce/GatorBrain/code/finetune/logs/%x.%j.out
+
+pwd; hostname; date
+
+module load singularity
+module load git
+
+
+
+
+singularity exec --writable /blue/ruogu.fang/jbroce/monaicore0.9.1 pip3 install --upgrade numpy
+
+singularity exec --nv /blue/ruogu.fang/jbroce/monaicore0.9.1 python3 -c "import torch; print('Found GPU') if torch.cuda.is_available() else print('Could not find GPU'); import sys; print(f'Python version {sys.version}'); print(torch.__version__)"
+singularity exec --nv --bind /red /blue/ruogu.fang/jbroce/monaicore0.9.1 python3 ./finetune.py
+
---
+++ code/finetune/.ipynb_checkpoints/trainer-checkpoint.py
@@ -0,0 +1,69 @@
+import torch
+from torch.utils.tensorboard import SummaryWriter
+from monai.data import DataLoader, ImageDataset
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+def run_training(model,
+    train_loader,
+    val_loader,
+    optimizer,
+    loss_function,
+    max_epochs):
+    val_interval = 2
+    best_metric = -1
+    best_metric_epoch = -1
+    epoch_loss_values = []
+    metric_values = []
+    writer = SummaryWriter()
+    for epoch in range(max_epochs):
+        print("-" * 10)
+        print(f"epoch {epoch + 1}/{max_epochs}")
+        model.train()
+        epoch_loss = 0
+        step = 0
+
+        for batch_data in train_loader:
+            step += 1
+            inputs, labels = batch_data[0].to(device), batch_data[1].to(device)
+            optimizer.zero_grad()
+            outputs = model(inputs)
+            loss = loss_function(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            epoch_loss += loss.item()
+            epoch_len = len(train_ds) // train_loader.batch_size
+            print(f"{step}/{epoch_len}, train_loss: {loss.item():.4f}")
+            writer.add_scalar("train_loss", loss.item(), epoch_len * epoch + step)
+
+        epoch_loss /= step
+        epoch_loss_values.append(epoch_loss)
+        print(f"epoch {epoch + 1} average loss: {epoch_loss:.4f}")
+
+        if (epoch + 1) % val_interval == 0:
+            model.eval()
+
+            num_correct = 0.0
+            metric_count = 0
+            for val_data in val_loader:
+                val_images, val_labels = val_data[0].to(device), val_data[1].to(device)
+                with torch.no_grad():
+                    val_outputs = model(val_images)
+                    value = torch.eq(val_outputs.argmax(dim=1), val_labels.argmax(dim=1))
+                    metric_count += len(value)
+                    num_correct += value.sum().item()
+
+            metric = num_correct / metric_count
+            metric_values.append(metric)
+
+            if metric > best_metric:
+                best_metric = metric
+                best_metric_epoch = epoch + 1
+                torch.save(model.state_dict(), "best_metric_model_classification3d_array.pth")
+                print("saved new best metric model")
+
+            print(f"Current epoch: {epoch+1} current accuracy: {metric:.4f} ")
+            print(f"Best accuracy: {best_metric:.4f} at epoch {best_metric_epoch}")
+            writer.add_scalar("val_accuracy", metric, epoch + 1)
+
+    print(f"Training completed, best_metric: {best_metric:.4f} at epoch: {best_metric_epoch}")
+    writer.close()
+    return best_metric
