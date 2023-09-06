97749b6cf47f8a7cc337c47a8b3b04e4bf27d540
Foundedd <founded@founded.gay>
2023-09-06 17:07:03 -0400

    debugging print statements

--- Downstream/BraTS/finetune.py
+++ Downstream/BraTS/finetune.py
@@ -99,16 +99,16 @@ def main_worker(args):
     num_epochs = args.epochs
     json_path = args.json_path
     num_workers = args.num_workers
-
+    print("Loaded args")
     hyperparameters = model_hyperparameters[model_path.split('/')[-1]]
     sw_batch_size = hyperparameters['out_channels'] * (
         hyperparameters['num_modalities'] if hyperparameters['batch_wise'] else 1)
-
+    print("Loaded hparams")
     if not torch.cuda.is_available():
         raise ValueError("CUDA enabled GPU is necessary for this code to run, sorry")  # ?
     device = torch.device(f"cuda:{args.local_rank}")
     torch.cuda.set_device(device)
-
+    print("Device located")
     train_loader, val_loader = get_loader(batch_size, data_dir, json_path, fold, roi, num_workers_=num_workers,
                                           rank=args.local_rank, world_size=dist.get_world_size())
 
@@ -122,9 +122,9 @@ def main_worker(args):
         dropout_path_rate=0.0,
         use_checkpoint=False,
     ).to(device)
-
+    print("Model sent to device")
     model = DistributedDataParallel(model, device_ids=[device])
-
+    print("Model passed through DDP")
     torch.backends.cudnn.benchmark = True  # Optimizes our runtime a lot
 
     loss = DiceLoss(to_onehot_y=False, sigmoid=True)
@@ -151,7 +151,7 @@ def main_worker(args):
         model.load_state_dict(checkpoint['model_state_dit'], strict=False)
     else:
         model.load_state_dict(checkpoint, strict=False)
-
+    print("Checkpoint loaded, starting training loop")
     val_acc_max, dices_tc, dices_wt, dices_et, dices_avg, loss_epochs, trains_epoch = \
         trainer(model,
                 train_loader=train_loader,
@@ -179,4 +179,5 @@ if __name__ == '__main__':
     finetune.py -d /red/ruogu.fang/brats -c ./models/finetune_cox_j.pt -e 200"""
     cl_args = parse_args()
     cl_args.local_rank = os.environ['LOCAL_RANK']
+    print("Spawning workers")
     main_worker(cl_args)
