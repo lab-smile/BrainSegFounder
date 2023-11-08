68bb44c7a1796e04afb43580017000b199555c32
coxjoseph <coxjoseph4004@gmail.com>
2023-11-08 12:23:18 -0500

    fixed json files

--- Downstream/BraTS/create_jsons.py
+++ Downstream/BraTS/create_jsons.py
@@ -1,4 +1,5 @@
 import json
+from copy import deepcopy
 from pathlib import Path
 from itertools import chain, combinations
 from typing import Iterable
@@ -23,14 +24,17 @@ def powerset(iterable: Iterable):
 
 def subset_modalities(folds_: list, modalities: set, all_modalities: set) -> list:
     removable_modalities = all_modalities - modalities
-    m_list = ['_' + name + '.nii.gz' for name in removable_modalities]
+    print(f'{removable_modalities = }')
+    m_list = [name + '.nii.gz' for name in removable_modalities]
+    print(f'{m_list}')
     subset_folds = []
     for fil_group in folds_:
         image_list = fil_group['image']
-        for image in image_list:
+        for image in deepcopy(image_list):
             modality = image.split('_')[-1]
             if modality in m_list:
                 image_list.remove(image)
+
 
         subset_folds.append({
             'fold': fil_group['fold'],
@@ -48,9 +52,11 @@ if __name__ == "__main__":
     folds = folds['training']  # The data inside the json file is all under "training" (as opposed to test)
     for desired_modalities in powerset(possible_modalities):
         desired_modalities = set(desired_modalities)
-        if not len(desired_modalities) == 0:  # Avoid empty set
+        if not (len(desired_modalities) == 0 or len(desired_modalities) == 4):  # Avoid empty set and full subset
+            print(desired_modalities)
             f_name = Path('./jsons') / name_set(desired_modalities)
-            new = subset_modalities(folds, desired_modalities, possible_modalities)
+            print(f_name)
+            new = subset_modalities(deepcopy(folds), desired_modalities, possible_modalities)
             new_folds = {'training': new}
             with open(f_name, 'w') as f:
                 json.dump(new_folds, f)
