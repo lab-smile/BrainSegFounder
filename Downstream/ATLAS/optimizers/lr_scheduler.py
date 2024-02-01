b8b9f667381e041b8fc0c49f6ee2c01180b7ee2a
coxjoseph <coxjoseph4004@gmail.com>
2024-02-01 13:34:43 -0500

    add warmupcosine to all

--- Downstream/ATLAS/optimizers/lr_scheduler.py
+++ Downstream/ATLAS/optimizers/lr_scheduler.py
@@ -17,7 +17,7 @@ from torch import nn as nn
 from torch.optim import Adam, Optimizer
 from torch.optim.lr_scheduler import LambdaLR, _LRScheduler
 
-__all__ = ["LinearLR", "ExponentialLR"]
+__all__ = ["LinearLR", "ExponentialLR", "WarmupCosineSchedule"]
 
 
 class _LRSchedulerMONAI(_LRScheduler):
