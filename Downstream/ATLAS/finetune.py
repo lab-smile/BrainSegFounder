b86aa62086d5eb2644c3b3f9109e970f6113064a
coxjoseph <coxjoseph4004@gmail.com>
2024-05-14 14:51:50 -0400

    prettier finetuning code

--- Downstream/ATLAS/finetune.py
+++ Downstream/ATLAS/finetune.py
@@ -1,13 +1,16 @@
 import os
 
 import bidsio
+import monai.networks.nets
+import numpy as np
+from monai.losses import DiceLoss
+from torch.cuda.amp import GradScaler, autocast
+
+from Downstream.ATLAS.training.lr_scheduler import WarmupCosineScheduler
 from data.split_data import get_split_indices
 import argparse
-import socket
-from urllib.parse import urlparse
 import torch
 import torch.distributed as dist
-from torch.utils.data import Subset, DataLoader, SequentialSampler, RandomSampler
 
 
 def parse_args() -> argparse.Namespace:
@@ -51,27 +54,9 @@ def parse_args() -> argparse.Namespace:
     return parser.parse_args()
 
 
-def find_valid_port(url: str) -> str:
-    parsed_url = urlparse(url)
-    hostname, port = parsed_url.hostname, parsed_url.port
-
-    if port is None:
-        port = 0
-
-    while True:
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-            try:
-                sock.bind((hostname, port))
-                sock.close()
-                break
-            except socket.error as e:
-                print(f'Port {port} cannot be bound with error {e}. Trying next port...')
-                port = 0
-    return f'{parsed_url.scheme}://{parsed_url.hostname}:{port}{parsed_url.path}'
-
-
-def trainer(distributed: bool = True, gpu: int = 0, backend: str = 'nccl',
-            init_methods: str = 'tcp://localhost:23456'):
+def trainer(gpu: int, indices: list[list[int]], dataset: bidsio.BIDSLoader, arguments: argparse.Namespace,
+            distributed: bool = True, backend: str = 'nccl',
+            url: str = 'tcp://localhost:23456', total_gpus: int = 1):
     if distributed:
         torch.multiprocessing.set_start_method('fork', force=True)
         rank = gpu
@@ -85,6 +70,140 @@ def trainer(distributed: bool = True, gpu: int = 0, backend: str = 'nccl',
     torch.cuda.set_device(gpu)
     torch.backends.cudnn.benchmark = True
 
+    print(f'Setup distributed training on GPU {gpu} (rank {rank})') if distributed else \
+        print('Setup single-GPU training')
+
+    if rank == 0:
+        print(f'Training with batch size: {arguments.batch_size} for {arguments.epochs}')
+
+    model = monai.networks.nets.SwinUNETR(in_channels=arguments.in_channels,
+                                          out_channels=arguments.out_channels,
+                                          feature_size=arguments.feature_size,
+                                          use_checkpoint=True,
+                                          depths=arguments.depths,
+                                          num_heads=arguments.num_heads,
+                                          drop_rate=arguments.dropout_rate)
+
+    if arguments.pretrained_model is not None:
+        original_weights = torch.load(arguments.pretrained_model)
+        state_dict = original_weights['state_dict']
+
+        if "module." in list(state_dict.keys())[0]:
+            if rank == 0:
+                print(f"[{rank}] Tag 'module.' found in state dict - fixing!")
+            for key in list(state_dict.keys()):
+                state_dict[key.replace("module.", "swinViT.")] = state_dict.pop(key)
+
+        # Not strict to only load encoder weights
+        model.load_state_dict(state_dict, strict=False)
+        if 'epoch' in original_weights:
+            model.epoch = original_weights["epoch"]
+        if 'optimizer' in original_weights:
+            model.optimizer = original_weights["optimizer"]
+        if rank == 0:
+            print(f"[GPU:{rank}]: Using pretrained backbone weights!")
+    model.to(device=gpu)
+
+    if arguments.optimizer == 'adam':
+        optimizer = torch.optim.Adam(params=model.parameters(), lr=arguments.learning_rate,
+                                     weight_decay=arguments.lr_decay)
+    elif arguments.optimizer == 'adamw':
+        optimizer = torch.optim.AdamW(params=model.parameters(), lr=arguments.learning_rate,
+                                      weight_decay=arguments.lr_decay)
+    else:
+        optimizer = torch.optim.SGD(params=model.parameters(), lr=arguments.learning_rate, momentum=arguments.momentum,
+                                    weight_decay=arguments.lr_decay)
+
+    if arguments.lr_decay:
+        if arguments.lr_scheduler == 'warmup_cosine':
+            scheduler = WarmupCosineScheduler(optimizer, warmup_steps=arguments.num_warmup_steps,
+                                              t_total=arguments.epochs)
+        else:
+            scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer,
+                                                          lr_lambda=(lambda e:
+                                                                     (1 - e / arguments.epochs) ** 0.9))
+    else:
+        scheduler = None
+
+    if arguments.distributed:
+        model = torch.nn.SyncBatchNorm.convert_sync_batchnorm(model)
+        model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[rank])
+
+    scaler = GradScaler() if arguments.amp else None
+    loss_function = DiceLoss()
+    best_loss = 1e8
+
+    train_idx, val_idx = indices
+    total_batches = (len(train_idx) + arguments.batch_size - 1) // arguments.batch_size
+
+    per_gpu = total_batches // total_gpus
+    leftovers = total_batches % total_gpus
+
+    start_batch = gpu * per_gpu + min(gpu, leftovers)
+    if gpu < leftovers:
+        per_gpu += 1
+
+    end_batch = start_batch + per_gpu
+    batched_indices = [indices[i * arguments.batch_size:(i + 1) * arguments.batch_size] for i in range(total_batches)]
+
+    # Select the batches that are assigned to the current GPU
+    gpu_batches = batched_indices[start_batch:end_batch]
+
+    print(f'[GPU: {rank}] Starting training!')
+    training_losses = []
+    validation_losses = []
+    for epoch in range(arguments.epochs):
+        print(f'[GPU:{rank}] Epoch {epoch}/{arguments.epochs}...')
+        model.train()
+        training_loss = []
+        for batch in gpu_batches:
+            image, label = dataset.load_batch(batch)
+            image = torch.Tensor(image, device=gpu)
+            label = torch.Tensor(label, device=gpu)
+
+            with autocast(enabled=arguments.amp):
+                preds = model(image)
+                loss = loss_function(preds, label)
+
+        training_loss.append(loss)
+        if arguments.amp:
+            scaler.scale(loss).backward()
+            optimizer.step()
+            scaler.step(optimizer)
+            scaler.update()
+        else:
+            loss.backward()
+        if arguments.lr_decay:
+            scheduler.step()
+        optimizer.zero_grad()
+
+        if epoch % 5 == 4 and gpu == 0:
+            print('Validating on GPU 0')
+            validation_loss = []
+            model.eval()
+            for index in val_idx:
+                image, label = train_loader.load_sample(index)
+                image = torch.Tensor(image, device=gpu)
+                label = torch.Tensor(label, device=gpu)
+
+                pred = model(image)
+                val_loss = loss_function(label, pred)
+
+                validation_loss.append(val_loss.item())
+            validation_loss = np.mean(validation_loss)
+            validation_losses.append(validation_loss)
+            print(f'Validation loss: {validation_loss}')
+            if validation_loss < best_loss:
+                torch.save(model, os.path.join(arguments.output, 'finetune_best_val_loss.pt'))
+                best_loss = validation_loss
+
+        total_loss = torch.tensor(training_loss)
+        dist.all_reduce(total_loss, op=dist.ReduceOp.SUM)
+        training_losses.append(total_loss / len(train_loader))
+
+    print(f'{training_losses=}')
+    print(f'Finetuning complete! Best validation loss: {best_loss}')
+    dist.destroy_process_group()
 
 
 if __name__ == '__main__':
@@ -103,9 +222,12 @@ if __name__ == '__main__':
                                      root_dir='data/train/')
 
     train_indices, validation_indices = get_split_indices(train_loader, split_fraction=0.8, seed=args.seed)
-    args.url = find_valid_port(args.url)
-
-
-
-
+    if args.distributed:
+        n_gpus = torch.cuda.device_count()
+        print(f'Found {n_gpus} accessible on each node.')
+        world_size = n_gpus * args.num_nodes
+        torch.multiprocessing.spawn(trainer, nprocs=n_gpus,
+                                    args=([train_indices, validation_indices],
+                                          train_loader, args, args.distributed,
+                                          args.backend, args.url, world_size))
 
