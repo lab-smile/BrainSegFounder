3ab32f6215b7a918fd25bc6c64957d8229ca7c8c
Joseph Cox <105464296+coxjoseph@users.noreply.github.com>
2024-08-12 17:25:15 -0400

    Update launch.sh

--- downstream/BraTS/finetuning/launch.sh
+++ downstream/BraTS/finetuning/launch.sh
@@ -30,7 +30,7 @@ optim_lr=1e-4
 logdir='log-'
 logdir+=$(date "+%Y-%m-%d-%H:%M:%S")
 
-singularity exec --bind /red --nv $path_to_singularity_container \
+singularity exec --bind /red --nv $path_to_singularity_container python main_FinetuningSwinUNETR_4Channels.py \
 --json_list=$path_to_json_list --distributed --data_dir=$path_to_data_dir --val_every=5 --noamp --pretrained_model_name=$path_to_checkpoint_dir \
 --pretrained_dir=$path_to_pretrained_dir --fold=$fold --roi_x=128 --roi_y=128 --roi_z=128 --in_channels=4 \
 --spatial_dims=3 --use_checkpoint --resume_ckpt --feature_size=48 --depths="$depths" --num_heads="$num_heads" --batch_size=$batch_size \
